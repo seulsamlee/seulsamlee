ee728ac8d77590a09ec3517139ee00aef31deacd
seulsam <152232786+seulsamlee@users.noreply.github.com>
2024-06-15 20:58:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const googleApiKey = 'AIzaSyA7lUva19lrCuSfCA1KZf4Ek_T_BHCjooU'; 
+export const googleApiKey = 'AIzaSyBAbxLmncuXiORlY2QCmUup199w10_llVY'; 
 export const googleSheetDocId = '1doRJyPxS8zZLeoq1CAxiOGMgetHnnCWdGZoepS0gm3U';
